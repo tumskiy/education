e60e1529675df60750656adf860f65cc7e88cc82
tumskiy <19vano93@gmail.com>
2022-01-28 20:30:10 +0300

    more letters for letter's God

--- les3/ms.js
+++ les3/ms.js
@@ -1,45 +1,41 @@
-function ms (array)
+function MergeSort (array)
 {
   if (array.length > 1)
   {
     let mid = Math.floor(array.length/2)
-    let lh = array.slice(0, mid);
-    let rh = array.slice(mid);
+    let LeftHalf = array.slice(0, mid);
+    let RightHalf = array.slice(mid);
 
-    ms(lh);
-    ms(rh);
+    MergeSort(LeftHalf);
+    MergeSort(RightHalf);
 
-    let l = 0;
-    let r = 0;
-    let k = 0;
-    while (l < lh.length && r < rh.length)
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let index = 0;
+    while (leftIndex < LeftHalf.length && rightIndex < RightHalf.length)
     {
-      if (lh[l] < rh [r])
+      if (LeftHalf[leftIndex] < RightHalf[rightIndex])
         {
-          array[k] = lh[l];
-          l++;
+          array[index] = LeftHalf[leftIndex];
+          leftIndex++;
         }
       else
         {
-          array[k] = rh[l];
-          r++;
+          array[index] = RightHalf[leftIndex];
+          rightIndex++;
         }
-        k++;
+        index++;
     }        
-    while (l < lh.length)
+    for(leftIndex < LeftHalf.length; (array[index] = LeftHalf[leftIndex]); leftIndex++)
     {
-      array[k] = lh[l];
-      l++;
-      k++;
+      index++;
     }
-    while (r < rh.length)
+    for(rightIndex < RightHalf.length; (array[index] = RightHalf[rightIndex]); rightIndex++)
     {
-      array[k] = rh[r];
-      r++;
-      k++;
+      index++;      
     }  
   }
   return array
 };
 let a = [45,12,77,8,46,78,23,1,8988,165,65536];
-console.log(ms(a));
+console.log(MergeSort(a));
